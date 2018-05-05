efa0dd7d227d2f4defdf2d2bc10b0f9a62b7e0cc
Clément Pit-Claudel <clement.pitclaudel@live.com>
2018-05-05 16:55:34 -0400

    Add pylint3 as an alternative name for pylint
    Closes GH-1437.

--- flycheck.el
+++ flycheck.el
@@ -8731,7 +8731,7 @@ CHECKER's executable is assumed to be a Python REPL."
   "Determines whether CHECKER needs to be invoked through Python.
 Previous versions of Flycheck called pylint and flake8 directly;
 this check ensures that we don't break existing code."
-  (not (string-match-p (rx (or "pylint" "flake8")
+  (not (string-match-p (rx (or "pylint" "pylint3" "flake8")
                            (or "-script.pyw" ".exe" ".bat" "")
                            eos)
                        (flycheck-checker-executable checker))))
